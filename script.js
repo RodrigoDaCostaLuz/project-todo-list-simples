1eb3f5b6439ecdf3a5b5606c23c55451266fe8f1
Rodrigo Luz <rodrigoluz999@gmail.com>
2023-01-07 19:15:48 -0300

    css e iniciando funções js

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 // Seleção de Elementos
+const inputTextoTarefa = document.getElementById('texto-tarefa');
+const btnCriarTarefa = document.getElementById('#criar-tarefa');
 
 // Funções
+const validaInput = () => inputTextoTarefa.value.trim().length > 0;
 
 // Eventos
+
+window.onload = () => {
+  validaInput();
+};

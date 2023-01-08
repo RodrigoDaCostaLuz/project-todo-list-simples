fd57fc791d32296bc889d0ed7ffe81334a615020
Rodrigo Luz <rodrigoluz999@gmail.com>
2023-01-08 12:50:13 -0300

    mais ajustes lint css e na função js

--- script.js
+++ script.js
@@ -5,13 +5,15 @@ const listaTarefas = document.getElementById('lista-tarefas');
 
 // add nova tarefa (li)
 const addNovaTarefa = () => {
-  const addTarefa = document.createElement('li');
-  addTarefa.classList.add('nova-tarefa');
-  addTarefa.innerHTML = inputTextoTarefa.value;
-  listaTarefas.appendChild(addTarefa);
-  inputTextoTarefa.value = '';
+  btnCriarTarefa.addEventListener('click', () => {
+    const addTarefa = document.createElement('li');
+    addTarefa.classList.add('nova-tarefa');
+    addTarefa.innerHTML = inputTextoTarefa.value;
+    listaTarefas.appendChild(addTarefa);
+    inputTextoTarefa.value = '';
+  });
 };
 
 window.onload = () => {
-  btnCriarTarefa.addEventListener('click', addNovaTarefa);
+  addNovaTarefa();
 };
